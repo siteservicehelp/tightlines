24c1f4e168ca51358d5a9175f39e16e5511355c9
siteservicehelp <siteservicehelp@gmail.com>
2025-09-18 13:15:23 +0300

    home page

---
+++ assets/js/home.js
@@ -0,0 +1,96 @@
+(function($) { $(function() {
+
+    // Preloader
+    $(window).on('load', function () {
+        preloader = $('.loaderArea'),
+        loader = preloader.find('.loader');
+        loader.fadeOut();
+        preloader.delay(350).fadeOut('slow');
+    });
+
+    // Кнопка Вверх
+    /*$(window).scroll(function(){
+        if ($(this).scrollTop() > 300) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+    $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+    // ScrollTo - Скролл до якоря #     
+    $('a.target-btn[href^="#"]').bind('click.smoothscroll',function (e) {
+        e.preventDefault();
+        var target = this.hash,
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - ($('header').height())
+        }, 600);
+    });*/ 
+
+}) })(jQuery)
+
+window.addEventListener('load', () => {
+   /**
+    * Sticky header
+    */
+    var header = document.querySelector('.header-wrapper');
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 30) {
+            header.classList.add('header-sticky');
+        } else {
+            header.classList.remove('header-sticky');
+        }
+    });
+
+
+   /*Menu toggle*/
+    const toggleButton = document.querySelector('.app-toggle-button')
+    const navMenu = document.querySelector('.menu-wrapper')
+    const bodyWrapper = document.querySelector('body')
+    const htmlWrapper = document.querySelector('html')
+    const headerWrapper = document.querySelector('.header-wrapper')
+   
+    if (toggleButton) { // add event listener only of button exists
+        toggleButton.addEventListener('click', function() {   
+            if(!this.classList.contains('button-open')) {
+                this.classList.add('button-open')
+                navMenu.classList.add('menu-wrapper-show')                
+                bodyWrapper.classList.add('overflow-hidden')
+                htmlWrapper.classList.add('overflow-hidden')
+                headerWrapper.classList.add('header-wrapper-open')
+            } else {
+                this.classList.remove('button-open')
+                navMenu.classList.remove('menu-wrapper-show')                
+                bodyWrapper.classList.remove('overflow-hidden')    
+                htmlWrapper.classList.remove('overflow-hidden')   
+                headerWrapper.classList.remove('header-wrapper-open')
+            }       
+        })
+    }
+
+
+
+    /*const menuItems = document.querySelectorAll('.menu-item-has-children')
+    menuItems.forEach(item => {
+        const itemLink = item.querySelector('a')
+        const itemMenu = item.querySelector('.sub-menu')
+        itemLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            this.classList.toggle('button-active');
+            itemMenu.classList.toggle('active');
+        });
+
+        document.addEventListener("click", (e) => {
+            let target = e.target;
+          
+            if (!item.contains(target) && itemMenu.classList.contains('active')) {
+                itemLink.classList.remove('button-active');
+                itemMenu.classList.remove('active');
+            }
+        })
+    });*/
+});
