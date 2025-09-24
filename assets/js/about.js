131f29a8736ac215954d9ad6bee46f8563a0c4a1
siteservicehelp <siteservicehelp@gmail.com>
2025-09-24 15:07:33 +0300

    about page

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,11 +1,17 @@
 window.addEventListener('load', () => {
   	var swiper = new Swiper(".teamSwiper", {
-    	slidesPerView: 3,
+    	slidesPerView: 1,
     	spaceBetween: 20,
     	loop: true,
         navigation: {
             nextEl: ".team-swiper-button-next",
             prevEl: ".team-swiper-button-prev",
         },
+        breakpoints: {
+        	576: {
+          		slidesPerView: 3,
+          		spaceBetween: 20,
+        	}
+      	},
   	});
 });
