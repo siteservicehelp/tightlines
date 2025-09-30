4cebc248d22fc4fcd4b91c0614fd664e73c29be8
siteservicehelp <siteservicehelp@gmail.com>
2025-09-30 16:36:47 +0300

    expeditions page

--- assets/js/expeditions.js
+++ assets/js/expeditions.js
@@ -7,12 +7,59 @@
 
 window.addEventListener('load', () => {
    /*Expeditions slider*/
-    var swiper = new Swiper(".expeditionsSwiper", {
+   var swiper = new Swiper(".single-expeditions-block", {
+        //loop: true,
+        spaceBetween: 20,
+        slidesPerView: 1,
+        grid: {
+        	fill: "row",
+	        rows: 1,
+	    },
+	    /*autoplay: {
+        	delay: 3000,
+        	disableOnInteraction: false,
+      	},*/
+      	pagination: {
+        	el: ".swiper-pagination",
+        	clickable: true,
+      	},
+      	breakpoints: {
+        	576: {
+          		slidesPerView: 2,
+          		spaceBetween: 20,
+          		grid: {
+		        	fill: "row",
+			        rows: 4,
+			    },
+        	},
+        	992: {
+          		slidesPerView: 3,
+          		spaceBetween: 20,
+          		grid: {
+		        	fill: "row",
+			        rows: 3,
+			    },
+        	},
+        	1441: {
+          		slidesPerView: 4,
+          		spaceBetween: 20,
+          		grid: {
+		        	fill: "row",
+			        rows: 2,
+			    },
+        	},
+      	},
+    });
+
+    var swiper2 = new Swiper(".expeditionsSwiper", {
         effect: "fade",
         loop: true,
         navigation: {
             nextEl: ".expeditions-swiper-button-next",
             prevEl: ".expeditions-swiper-button-prev",
+        },
+        thumbs: {
+            swiper: swiper,
         },
     });
 });
